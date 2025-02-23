0dc636fccdce168331e9603676d20ec6344cf01f
Jegge2003 <jamesoappiah2003@gmail.com>
2025-02-23 15:56:47 +0000

    Make changes

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,16 +1,18 @@
 from django.shortcuts import render
-from django.views.generic.detail import DetailView  # Correct import
-from .models import Book, Library
+from django.views.generic.detail import DetailView
+from .models import Library
+from .models import Book
 
-# Function-Based View (FBV) - Lists all books
+# Create your views here.
 def list_books(request):
     """Function-based view to list all books with their authors"""
     books = Book.objects.all()
-    return render(request, 'list_books.html', {'books': books})  # ✅ No "relationship_app/" prefix
+    return render(request, 'relationship_app/list_books.html', {'books': books})
 
-# Class-Based View (CBV) - Displays details for a specific library
+
 class LibraryDetailView(DetailView):
     """Class-based view to display details of a specific library"""
     model = Library
-    template_name = 'library_detail.html'  # ✅ No "relationship_app/" prefix
+    template_name = 'relationship_app/library_detail.html'
     context_object_name = 'library'
+
