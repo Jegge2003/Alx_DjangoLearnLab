197c7fda1e1b59b9db1406b95941fc3d2f421032
Jegge2003 <jamesoappiah2003@gmail.com>
2025-02-23 16:31:34 +0000

    Added some important links

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -26,6 +26,6 @@ urlpatterns = [
     path('books/', list_books, name='list_books'),  # Function-based view
     path('library/<int:pk>/', LibraryDetailView.as_view(), name='library_detail'),  # Class-based view
     path('register/', views.register, name='register'),  # ✅ User registration
-    path('login/', LoginView.as_view(template_name='login.html'), name='login'),  # ✅ Built-in login view with custom template
-    path('logout/', LogoutView.as_view(template_name='logout.html'), name='logout'), 
+    path('login/', LoginView.as_view(template_name='relationship_app/login.html'), name='login'),  # ✅ Built-in login view with custom template
+    path('logout/', LogoutView.as_view(template_name='relationship_app/logout.html'), name='logout'), 
 ]
