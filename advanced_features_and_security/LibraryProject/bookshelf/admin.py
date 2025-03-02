edf238afe999261241fbba8eb1a48c4899b66bc8
Jegge2003 <jamesoappiah2003@gmail.com>
2025-03-02 16:56:33 +0000

    Modified bookshelf

--- advanced_features_and_security/LibraryProject/bookshelf/admin.py
+++ advanced_features_and_security/LibraryProject/bookshelf/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import CustomUser
 from .models import Book
 
 # Register your models here.
@@ -9,3 +11,22 @@ class BookAdmin(admin.ModelAdmin):
     search_fields = ('title', 'author')
     list_filter = ('publication_year',)
 
+
+class CustomUserAdmin(UserAdmin):
+    """Custom admin interface for managing users"""
+    model = CustomUser
+    list_display = ('username', 'email', 'date_of_birth', 'is_staff', 'is_active')
+    fieldsets = (
+        (None, {'fields': ('username', 'email', 'password', 'profile_photo', 'date_of_birth')}),
+        ('Permissions', {'fields': ('is_staff', 'is_active', 'is_superuser', 'groups', 'user_permissions')}),
+    )
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('username', 'email', 'password1', 'password2', 'profile_photo', 'date_of_birth', 'is_staff', 'is_active')}
+        ),
+    )
+    search_fields = ('email', 'username')
+    ordering = ('email',)
+
+admin.site.register(CustomUser, CustomUserAdmin)
