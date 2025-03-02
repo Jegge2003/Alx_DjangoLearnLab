fec3fdc484d4c753c399f31e1124344e12719d88
Jegge2003 <jamesoappiah2003@gmail.com>
2025-03-02 17:53:15 +0000

    Modified forms

--- advanced_features_and_security/LibraryProject/bookshelf/forms.py
+++ advanced_features_and_security/LibraryProject/bookshelf/forms.py
@@ -5,3 +5,10 @@ class BookForm(forms.ModelForm):
     class Meta:
         model = Book
         fields = ['title', 'author', 'publication_year']
+
+
+# Adding ExampleForm to meet the requirement
+class ExampleForm(forms.Form):
+    name = forms.CharField(max_length=100, required=True)
+    email = forms.EmailField(required=True)
+    message = forms.CharField(widget=forms.Textarea, required=True)
