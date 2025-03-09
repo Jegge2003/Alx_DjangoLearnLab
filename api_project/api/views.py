e1cb803dc80d963171a8821f2014fec1ff560546
Jegge2003 <jamesoappiah2003@gmail.com>
2025-03-09 18:49:39 +0000

    Modified some files in api project

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -3,10 +3,15 @@ from django.shortcuts import render
 # Create your views here.
 # api/views.py
 
-from rest_framework import generics
+from rest_framework import generics, viewsets
 from .models import Book
 from .serializers import BookSerializer
 
 class BookList(generics.ListAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+
+# ViewSet for full CRUD functionality
+class BookViewSet(viewsets.ModelViewSet):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
