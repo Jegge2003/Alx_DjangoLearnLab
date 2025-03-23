4eb09a283a37891e6468b927d2cdad2a783080b1
Jegge2003 <jamesoappiah2003@gmail.com>
2025-03-23 16:46:41 +0000

    Made some changes

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -1,3 +1,41 @@
-from django.shortcuts import render
+# blog/views.py
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
+from django import forms
 
-# Create your views here.
+
+def home(request):
+    return render(request, 'blog/home.html')
+
+# Extend UserCreationForm to add email
+class CustomUserCreationForm(UserCreationForm):
+    email = forms.EmailField(required=True)
+
+    class Meta:
+        model = User
+        fields = ("username", "email", "password1", "password2")
+
+def register(request):
+    if request.method == 'POST':
+        form = CustomUserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)  # Auto login after registration
+            return redirect('profile')
+    else:
+        form = CustomUserCreationForm()
+    return render(request, 'registration/register.html', {'form': form})
+
+
+@login_required
+def profile(request):
+    if request.method == 'POST':
+        user = request.user
+        user.email = request.POST.get('email')
+        user.save()
+        return redirect('profile')
+
+    return render(request, 'registration/profile.html')
