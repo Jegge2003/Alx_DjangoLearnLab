186dc9f6c0f43c34f56a4eca766ba231b2e28088
Jegge2003 <jamesoappiah2003@gmail.com>
2025-03-23 16:58:15 +0000

    Made some changes

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -20,7 +20,7 @@ from . import views
 from django.contrib.auth import views as auth_views
 
 urlpatterns = [
-    path('', views.home, name='home'),  # This catches the empty path
+    #path('', views.home, name='home'),  # This catches the empty path
     path('register/', views.register, name='register'),
     path('profile/', views.profile, name='profile'),
     path('login/', auth_views.LoginView.as_view(template_name='registration/login.html'), name='login'),
