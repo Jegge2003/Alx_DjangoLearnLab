4c149be9e00c6bf8db7fe51e8212cb93476272cd
Jegge2003 <jamesoappiah2003@gmail.com>
2025-03-23 17:27:03 +0000

    Added files

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -29,7 +29,7 @@ urlpatterns = [
     path('post/', PostListView.as_view(), name='post-list'),
     path('post/new/', PostCreateView.as_view(), name='post-create'),
     path('post/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
-    path('post/<int:pk>/edit/', PostUpdateView.as_view(), name='post-update'),
+    path('post/<int:pk>/update/', PostUpdateView.as_view(), name='post-update'),
     path('post/<int:pk>/delete/', PostDeleteView.as_view(), name='post-delete'),
     # Add this if you have a posts page:
     # path('posts/', views.posts_view, name='posts'),
