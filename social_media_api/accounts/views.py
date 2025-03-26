1f69a88f2909e79a9790ab5df4219791693072c6
Jegge2003 <jamesoappiah2003@gmail.com>
2025-03-26 22:26:39 +0000

    Added some files

--- social_media_api/accounts/views.py
+++ social_media_api/accounts/views.py
@@ -5,6 +5,9 @@ from .serializers import RegisterSerializer, LoginSerializer, ProfileSerializer
 from rest_framework.response import Response
 from rest_framework.authtoken.models import Token
 from rest_framework.views import APIView
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated
+from django.shortcuts import get_object_or_404
 
 # Create your views here.
 class RegisterView(generics.CreateAPIView):
@@ -25,3 +28,20 @@ class ProfileView(generics.RetrieveUpdateAPIView):
 
     def get_object(self):
         return self.request.user
+    
+
+@api_view(['POST'])
+@permission_classes([IsAuthenticated])
+def follow_user(request, user_id):
+    target_user = get_object_or_404(User, id=user_id)
+    if target_user == request.user:
+        return Response({"detail": "You cannot follow yourself."}, status=400)
+    request.user.following.add(target_user)
+    return Response({"detail": f"You are now following {target_user.username}."})
+
+@api_view(['POST'])
+@permission_classes([IsAuthenticated])
+def unfollow_user(request, user_id):
+    target_user = get_object_or_404(User, id=user_id)
+    request.user.following.remove(target_user)
+    return Response({"detail": f"You have unfollowed {target_user.username}."})
