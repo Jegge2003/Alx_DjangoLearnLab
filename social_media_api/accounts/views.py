7336fe276900d728c2a73c8434a1912e1b6c3333
Jegge2003 <jamesoappiah2003@gmail.com>
2025-03-27 20:43:51 +0000

    Made changes to views

--- social_media_api/accounts/views.py
+++ social_media_api/accounts/views.py
@@ -1,4 +1,5 @@
-from rest_framework import generics, status, permissions
+from rest_framework import generics, status 
+from rest_framework import permissions
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework.authtoken.models import Token
