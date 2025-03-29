7a1b671e950caafb605ed00cfd28241978137ea3
Jegge2003 <jamesoappiah2003@gmail.com>
2025-03-29 06:07:00 +0000

    Modified views in post app

--- social_media_api/posts/views.py
+++ social_media_api/posts/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render
+from rest_framework.generics import get_object_or_404
 from rest_framework import viewsets, permissions, filters
 from .models import Post, Comment
 from .serializers import PostSerializer, CommentSerializer
